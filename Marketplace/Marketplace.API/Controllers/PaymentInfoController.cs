a4196179688790cccf8687b7bea6303f0a115cf2
Ian Honorato <ian.pereira@basis.com.br>
2023-06-28 16:00:23 -0300

    todos os cruds implementados

---
+++ Marketplace/Marketplace.API/Controllers/PaymentInfoController.cs
@@ -0,0 +1,60 @@
+﻿using Marketplace.Interfaces.Services;
+using Marketplace.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Marketplace.API.Controllers
+{
+    public class PaymentInfoController : Controller
+    {
+        private readonly IPaymentInfoService _paymentInfoService;
+        public PaymentInfoController(IPaymentInfoService paymentInfoService)
+        {
+            _paymentInfoService = paymentInfoService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("payment-info/{id}")]
+        public async Task<ActionResult<PaymentInfoResponseDto>> GetPaymentInfoById(int id)
+        {
+            var paymentInfo = await _paymentInfoService.GetPaymentInfoByIdAsync(id);
+            if (paymentInfo == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(paymentInfo);
+        }
+
+        [HttpGet("payment-info")]
+        public async Task<ActionResult<List<PaymentInfoResponseDto>>> GetAllPaymentInfo()
+        {
+            var paymentInfo = await _paymentInfoService.GetAllPaymentInfos();
+            return Ok(paymentInfo);
+        }
+
+        [HttpPost("payment-info")]
+        public async Task<ActionResult<int>> CreatePaymentInfo(PaymentInfoCreateDto paymentInfo)
+        {
+            var paymentInfoId = await _paymentInfoService.SavePaymentInfo(paymentInfo);
+            return CreatedAtAction(nameof(GetPaymentInfoById), new { id = paymentInfoId }, paymentInfoId);
+        }
+
+        [HttpPut("payment-info")]
+        public async Task<ActionResult> UpdatePaymentInfo(PaymentInfoUpdateDto paymentInfo)
+        {
+            await _paymentInfoService.UpdatePaymentInfo(paymentInfo);
+            return NoContent();
+        }
+
+        [HttpDelete("payment-info/{id}")]
+        public async Task<ActionResult> DeletePaymentInfo(int id)
+        {
+            await _paymentInfoService.DeletePaymentInfo(id);
+            return NoContent();
+        }
+    }
+}
---
+++ Marketplace/Marketplace.API/Controllers/ProductReviewController.cs
@@ -0,0 +1,61 @@
+﻿using Marketplace.Interfaces.Services;
+using Marketplace.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Marketplace.API.Controllers
+{
+    public class ProductReviewController : Controller
+    {
+        private readonly IProductReviewService _productReviewService;
+
+        public ProductReviewController(IProductReviewService productReviewService)
+        {
+            _productReviewService = productReviewService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("product-review/{id}")]
+        public async Task<ActionResult<ProductReviewResponseDto>> GetProductReviewById(int id)
+        {
+            var productReview = await _productReviewService.GetProductReviewByIdAsync(id);
+            if (productReview == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(productReview);
+        }
+
+        [HttpGet("product-review")]
+        public async Task<ActionResult<List<ProductReviewResponseDto>>> GetAllProductReviews()
+        {
+            var reviews = await _productReviewService.GetAllReviews();
+            return Ok(reviews);
+        }
+
+        [HttpPost("product-review")]
+        public async Task<ActionResult<int>> CreateProductReview(ProductReviewCreateDto productReview)
+        {
+            var productReviewId = await _productReviewService.SaveProductReview(productReview);
+            return CreatedAtAction(nameof(GetProductReviewById), new { id = productReviewId}, productReviewId);
+        }
+
+        [HttpPut("product-review")]
+        public async Task<ActionResult> UpdateProductReview(ProductReviewUpdateDto productReview)
+        {
+            await _productReviewService.UpdateProductReview(productReview);
+            return NoContent();
+        }
+
+        [HttpDelete("product-review/{id}")]
+        public async Task<ActionResult> DeleteProductReview(int id)
+        {
+            await _productReviewService.DeleteProductReview(id);
+            return NoContent();
+        }
+    }
+}
